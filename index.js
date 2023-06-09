aa2f4a8c31b4b3a033ea391c0ae2e00e0cb50f80
Izyn <izinnoushad@gmail.com>
2023-06-09 15:40:36 +0530

    index.js is added

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require("node:http");
+const fs = require("node:fs");
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {"Const-Type": "text/html"});
+    const html =fs.readFileSync("./index.html", "utf-8");
+    res.end(html);
+});
+
+server.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
